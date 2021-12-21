be9aa8c93cad91efdc7db1b963b04320b8db923b
DeNeMiX83 <daniil53891@yandex.ru>
2021-12-21 23:12:36 +0300

    fix add static files

--- SmoothieFeed/settings.py
+++ SmoothieFeed/settings.py
@@ -19,9 +19,17 @@ from pathlib import Path
 import dj_database_url
 
 load_dotenv()
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
