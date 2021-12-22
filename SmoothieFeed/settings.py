40fc4e338c90936c933c4e0a1d2b3ae9feaa92f3
DeNeMiX83 <daniil53891@yandex.ru>
2021-12-22 18:42:43 +0300

    fix add static files

--- SmoothieFeed/settings.py
+++ SmoothieFeed/settings.py
@@ -22,22 +22,12 @@ import dj_database_url
 load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-django_heroku.settings(locals())
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 try:
     SECRET_KEY = os.environ["SECRET_KEY"]
 except KeyError:
@@ -202,3 +192,6 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+
+django_heroku.settings(locals())
